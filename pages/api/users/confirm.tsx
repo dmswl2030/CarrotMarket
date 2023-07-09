d40d1af3849e3cf777a96ffff52e632d63ae50c9
EUNJI <eunji9180@naver.com>
2023-07-10 03:00:51 +0900

    :heavy_plus_sign: Profile Handler, Add Iron-session

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,28 +1,35 @@
 import { withIronSessionApiRoute } from "iron-session/next";
-import client from "@libs/server/client";
-import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  console.log(req.session.save);
   const { token } = req.body;
   const exists = await client.token.findUnique({
     where: {
       payload: token,
     },
   });
-  if (!exists) res.status(404).end();
+  if (!exists) return res.status(404).end();
   req.session.user = {
-    id: exists?.userId,
+    id: exists.userId,
   };
   await req.session.save();
   res.status(200).end();
 }
-
 export default withIronSessionApiRoute(withHandler("POST", handler), {
   cookieName: "carrotsession",
-  password: "841902839058129058874182374981234",
+  password:
+    "9845904809485098594385093840598df;slkgjfdl;gkfsdjg;ldfksjgdsflgjdfklgjdflgjflkgjdgd",
 });
