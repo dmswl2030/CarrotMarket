8c6d9478e1f5f99c4b242a078dd5c2b0cfc92541
EUNJI <eunji9180@naver.com>
2023-07-11 08:00:48 +0900

    :recycle: UseSWR, SWRConfig

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -7,8 +7,11 @@ const fetcher = (url: string) => fetch(url).then((response) => response.json());
 export default function useUser() {
   const { data, error } = useSWR("/api/users/me", fetcher);
   const router = useRouter();
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
 
-  //         return router.replace("/enter");
-
-  return data;
+  return { user: data?.profile, isLoading: !data && !error };
 }
